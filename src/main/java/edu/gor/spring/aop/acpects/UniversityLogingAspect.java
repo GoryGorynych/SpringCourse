fac520df45839909b17d6c44c46d82c1adc7d18e
Vladimit <gory.gorynych@gmail.com>
2022-07-02 12:32:10 +0300

    blabla

--- src/main/java/edu/gor/spring/aop/acpects/UniversityLogingAspect.java
+++ src/main/java/edu/gor/spring/aop/acpects/UniversityLogingAspect.java
@@ -1,9 +1,7 @@
 package edu.gor.spring.aop.acpects;
 
 import edu.gor.spring.aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,24 +10,35 @@ import java.util.List;
 @Aspect
 public class UniversityLogingAspect {
 
-    @Before("execution(* getStudents())")
-    public void beforeGetStudentsLoggingAdvice() {
-        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение списка студентов перед методом getStudents");
-    }
+//    @Before("execution(* getStudents())")
+//    public void beforeGetStudentsLoggingAdvice() {
+//        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение списка студентов перед методом getStudents");
+//    }
 
-    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
-    public void afterGetStudentsLoggingAdvice(List<Student> students) {
+//    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
+//    public void afterGetStudentsLoggingAdvice(List<Student> students) {
+//
+//        Student firstSt = students.get(0);
+//
+//        String nameSurname = firstSt.getNameSurname();
+//        nameSurname = "Mr. " + nameSurname;
+//        firstSt.setNameSurname(nameSurname);
+//
+//        double avgGrade = firstSt.getAvgGrade();
+//        avgGrade = avgGrade+1;
+//        firstSt.setAvgGrade(avgGrade);
+//
+//        System.out.println("afterGetStudentsLoggingAdvice: логируем получение списка студентов после работы метода getStudents");
+//    }
 
-        Student firstSt = students.get(0);
+//    @AfterThrowing(pointcut = "execution (* getStudents())", throwing = "exception")
+//    public void afterThrowGetStudentsLoggingAdvice(Throwable exception) {
+//
+//        System.out.println("afterThrowGetStudentsLoggingAdvice: логируем выброс исключения " + exception);
+//    }
 
-        String nameSurname = firstSt.getNameSurname();
-        nameSurname = "Mr. " + nameSurname;
-        firstSt.setNameSurname(nameSurname);
-
-        double avgGrade = firstSt.getAvgGrade();
-        avgGrade = avgGrade+1;
-        firstSt.setAvgGrade(avgGrade);
-
-        System.out.println("afterGetStudentsLoggingAdvice: логируем получение списка студентов после работы метода getStudents");
+    @After("execution(* getStudents())")
+    public void afterGetStudentsLoggingAdvice() {
+        System.out.println("afterGetStudentsLoggingAdvice: логируем нормальное окончание работы метода или выброс исключения");
     }
 }
